e1eebc993869758971a69e802a59b238b4e1a031
Victor <victor.msousa41@gmail.com>
2026-08-11 13:56:19 -0300

    refactor: uncomment and enable binary conversion logic for user input processing

--- Converter.py
+++ Converter.py
@@ -1,22 +1,22 @@
-# def para_binario(simbolo):
-#     numero_tabela = ord(simbolo)
-#     codigo_binário = format(numero_tabela, '08b')
-#     return codigo_binário
+def para_binario(simbolo):
+    numero_tabela = ord(simbolo)
+    codigo_binário = format(numero_tabela, '08b')
+    return codigo_binário
 
-# def texto_para_binario(texto):
-#     resultado_final = []
+def texto_para_binario(texto):
+    resultado_final = []
     
     
-#     for letra in texto:
-#         binario = para_binario(letra)
-#         resultado_final.append(binario)
+    for letra in texto:
+        binario = para_binario(letra)
+        resultado_final.append(binario)
         
    
-#     return " ".join(resultado_final)
+    return " ".join(resultado_final)
 
-# mensagem = input("Informe a frase: ")
-# print(f"'{mensagem}' em binário fica:")
-# print(texto_para_binario(mensagem))
+mensagem = input("Informe a frase: ")
+print(f"'{mensagem}' em binário fica:")
+print(texto_para_binario(mensagem))
 
 #Função trasnformar uma imagem para binário
 
